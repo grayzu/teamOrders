63fa074eab3af45d11130e17ad75675bd75b94e4
MarkG <mark@grayzu.com>
2017-01-18 15:39:27 -0800

    patially updated shock dialog

--- client/modalDialog.js
+++ client/modalDialog.js
@@ -1,5 +1,198 @@
 Template.genericModalDialog.helpers({
-
+    shockInfo: [
+        {
+            "Bike": "V10C 26in",
+            "Bike travel": "8.5/10",
+            "Tune": "M/M",
+            "Air can": "(vivid only) ",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 9.45,
+            "Stroke": 2.99,
+            "E2E mm": 240,
+            "Stroke mm": 76,
+            "Leverage Ratio": "3.6:1"
+        },
+        {
+            "Bike": "V10c 650B",
+            "Bike travel": "216mm",
+            "Tune": "M/M",
+            "Air can": "(vivid only) ",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 8.74,
+            "Stroke": 2.76,
+            "E2E mm": 222,
+            "Stroke mm": 70,
+            "Leverage Ratio": ""
+        },
+        {
+            "Bike": "Nomad 1",
+            "Bike travel": "165mm",
+            "Tune": "M/M",
+            "Air can": "DebonAir",
+            "Upper and Lower hardware": "3/8 x22mm",
+            "Eye to eye": 8.5,
+            "Stroke": 2.48,
+            "E2E mm": 216,
+            "Stroke mm": 63,
+            "Leverage Ratio": "2.6:1"
+        },
+        {
+            "Bike": "Nomad 2",
+            "Bike travel": "165mm",
+            "Tune": "M/M",
+            "Air can": "DebonAir",
+            "Upper and Lower hardware": "8 x22mm",
+            "Eye to eye": 8.5,
+            "Stroke": 2.48,
+            "E2E mm": 216,
+            "Stroke mm": 63,
+            "Leverage Ratio": "2.54:1"
+        },
+        {
+            "Bike": "Nomad 3",
+            "Bike travel": "165mm",
+            "Tune": "M/M",
+            "Air can": "DebonAir",
+            "Upper and Lower hardware": "8 x22mm",
+            "Eye to eye": 8.5,
+            "Stroke": 2.48,
+            "E2E mm": 216,
+            "Stroke mm": 63,
+            "Leverage Ratio": "2.6:1"
+        },
+        {
+            "Bike": "Bronson (all)",
+            "Bike travel": "150mm",
+            "Tune": "M/M",
+            "Air can": "DebonAir",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 7.87,
+            "Stroke": 2.24,
+            "E2E mm": 200,
+            "Stroke mm": 57,
+            "Leverage Ratio": "2.63:1"
+        },
+        {
+            "Bike": "Tallboy",
+            "Bike travel": "100mm",
+            "Tune": "M/M",
+            "Air can": "Standard Solo Air",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 6.5,
+            "Stroke": 1.5,
+            "E2E mm": 165,
+            "Stroke mm": 38,
+            "Leverage Ratio": "2.63:1"
+        },
+        {
+            "Bike": "d8",
+            "Bike travel": "8",
+            "Tune": "M/M",
+            "Air can": "DebonAir",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 8.75,
+            "Stroke": 2.75,
+            "E2E mm": 222.25,
+            "Stroke mm": 69.85,
+            "Leverage Ratio": "2.9:1"
+        },
+        {
+            "Bike": "aluminum V10",
+            "Bike travel": "8.5/10",
+            "Tune": "M/M",
+            "Air can": "(vivid only) ",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 8.75,
+            "Stroke": 2.75,
+            "E2E mm": 222.25,
+            "Stroke mm": 69.85,
+            "Leverage Ratio": "3.6:1"
+        },
+        {
+            "Bike": "Tallboy 3",
+            "Bike travel": "110mm",
+            "Tune": "M/M",
+            "Air can": "Standard Solo Air",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 7.25,
+            "Stroke": 1.75,
+            "E2E mm": null,
+            "Stroke mm": null,
+            "Leverage Ratio": ""
+        },
+        {
+            "Bike": "Tallboy LT",
+            "Bike travel": "135mm",
+            "Tune": "M/M",
+            "Air can": "Standard Solo Air or Debonair",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 7.87,
+            "Stroke": 2.01,
+            "E2E mm": 200,
+            "Stroke mm": 51,
+            "Leverage Ratio": "2.7:1"
+        },
+        {
+            "Bike": "Hightower",
+            "Bike travel": "135mm",
+            "Tune": "M/M",
+            "Air can": "Standard Solo Air or Debonair",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 7.87,
+            "Stroke": 2.01,
+            "E2E mm": 200,
+            "Stroke mm": 51,
+            "Leverage Ratio": ""
+        },
+        {
+            "Bike": "5010",
+            "Bike travel": "130mm",
+            "Tune": "M/M",
+            "Air can": "Standard Solo Air or Debonair",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 7.87,
+            "Stroke": 2.01,
+            "E2E mm": 200,
+            "Stroke mm": 51,
+            "Leverage Ratio": "2.5:1"
+        },
+        {
+            "Bike": "Solo",
+            "Bike travel": "130mm",
+            "Tune": "M/M",
+            "Air can": "Standard Solo Air or Debonair",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 7.87,
+            "Stroke": 2.01,
+            "E2E mm": 200,
+            "Stroke mm": 51,
+            "Leverage Ratio": ""
+        },
+        {
+            "Bike": "Blur LT 1",
+            "Bike travel": "150mm",
+            "Tune": "M/M",
+            "Air can": "Standard Solo Air or Debonair",
+            "Upper and Lower hardware": "8 x22",
+            "Eye to eye": 7.87,
+            "Stroke": 2.01,
+            "E2E mm": 200,
+            "Stroke mm": 51,
+            "Leverage Ratio": "2.36:1"
+        },
+        {
+            "Bike": "Blur LT 2 2008-2012",
+            "Bike travel": "150mm",
+            "Tune": "M/M",
+            "Air can": "Standard Solo Air or Debonair",
+            "Upper and Lower hardware": "9 x22",
+            "Eye to eye": 8.5,
+            "Stroke": 2.5,
+            "E2E mm": null,
+            "Stroke mm": null,
+            "Leverage Ratio": "2.2:1"
+        }
+    ]
 });
 
 Template.genericModalDialog.events({
@@ -31,5 +224,8 @@ Template.genericModalDialog.events({
             Modal.hide();
         }
         
+    },
+    'change #bikeModel': function(evt, tmpl){
+        alert(tmpl.$("#bikeModel").val());
     }
 });
--- client/product.js
+++ client/product.js
@@ -8,56 +8,7 @@ Template.product.events({
         }
         else {
             var body = `
-                        <form class="form-inline">
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth"  for="model">Bike Model / Travel:</label>
-                                <input type="text" class="form-control fullwidth" id="model">
-                            </div>
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth" for="e2e">Eye-to-eye:</label>
-                                <input type="text" class="form-control fullwidth" id="e2e">
-                            </div>
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth" for="stroke">Stroke:</label>
-                                <input type="text" class="form-control fullwidth" id="stroke">
-                            </div>
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth" for="tune">Tune:</label>
-                                <input type="text" class="form-control fullwidth" id="tune">
-                            </div>
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth" for="airCan">Air can:</label>
-                                <input type="text" class="form-control fullwidth" id="airCan">
-                            </div>
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth" for="volumespacer">Volume spacers:</label>
-                                <input type="text" class="form-control fullwidth" id="volumespacer">
-                            </div>
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth" for="upperhardware">Upper hardware:</label>
-                                <input type="text" class="form-control fullwidth" id="upperhardware">
-                            </div>
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth" for="lowerhardware">Lower hardware:</label>
-                                <input type="text" class="form-control fullwidth" id="lowerhardware">
-                            </div>
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth" for="coilspring">Coil spring:</label>
-                                <input type="text" class="form-control fullwidth" id="coilspring">
-                            </div>
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth" for="leverageratio">Leverage ratio:</label>
-                                <input type="text" class="form-control fullwidth" id="leverageratio">
-                            </div>
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth" for="averageleverageratio">Average leverage ratio:</label>
-                                <input type="text" class="form-control fullwidth" id="averageleverageratio">
-                            </div>
-                            <div class="form-group shockaddedinfo">
-                                <label class="fullwidth" for="leverageratiosag">Leverage ratio sag:</label>
-                                <input type="text" class="form-control fullwidth" id="leverageratiosag">
-                            </div>
-                        </form>            
+                        //not used
             `
             Modal.show('genericModalDialog',{Title: 'Rear Shock Additional Details', AddtoCart: true, body: body, qty: tmpl.$('.itemQty').val(), data: tmpl.data});
         }
